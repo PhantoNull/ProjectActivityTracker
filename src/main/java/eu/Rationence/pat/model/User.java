351ef7776192684f99a1a517b538a6c87e479ea3
PhantoNull <l.dipierro@campus.unimib.it>
2022-03-12 00:21:17 +0100

    Initial commit

---
+++ src/main/java/eu/Rationence/pat/model/User.java
@@ -0,0 +1,44 @@
+package eu.Rationence.pat.model;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+
+@Entity(name = "User")
+@Table(name = "User")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @Column(name="c_UserName", length=32, unique = true, nullable = false)
+    private String username;
+
+    @Column(name="e_Email", unique = true, nullable = false)
+    @Email
+    private String email;
+
+    @Column(name="x_Name", nullable = false, length=32)
+    private String name;
+
+    @Column(name="x_Surame", nullable = false, length=64)
+    private String surname;
+
+    @Column(name="c_Role", nullable = false, length=16)
+    private String role;
+
+    @Column(name="i_Cost", nullable = false)
+    private float cost;
+
+    @Column(name="x_Time", nullable = false, length=5)
+    private String time;
+
+    @Column(name="h_Password", nullable = false, length=255)
+    private String passwordHash;
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="h_Salt", length=5, nullable = false)
+    private String passwordSalt;
+}
---
+++ src/test/java/eu/Rationence/pat/PatApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.Rationence.pat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
