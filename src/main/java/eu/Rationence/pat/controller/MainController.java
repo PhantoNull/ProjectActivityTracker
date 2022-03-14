c239479a78eb2828b7bd2b9d2ee151c1624fc5c8
PhantoNull <lphanto90@gmail.com>
2022-03-14 11:23:38 +0100

    creazione branch develop + aggiunta immagini

--- src/main/java/eu/Rationence/pat/controller/MainController.java
+++ src/main/java/eu/Rationence/pat/controller/MainController.java
@@ -2,13 +2,20 @@ package eu.Rationence.pat.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 
+
+    @RequestMapping ("/")
+    public String index() {
+        return "index.html";
+    }
+
     // Login form
-    @RequestMapping("/login.html")
+    @RequestMapping("/login")
     public String login() {
         return "login.html";
     }
