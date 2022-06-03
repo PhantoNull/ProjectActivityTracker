c2554ba41147299169f1bb5f67b2e0c13c22fbfb
PhantoNull <l.dipierro@campus.unimib.it>
2022-06-03 16:28:26 +0200

    Modificata funzione export-excel.js per esportare file con nome, vanno presi i valori dalla tabella manualmente per costruire l'excel

--- src/main/resources/public/js/export-excel.js
+++ src/main/resources/public/js/export-excel.js
@@ -1,4 +1,4 @@
-function extractXLSX(){
+function extractXLSX(month, year, name){
     let table_elt = document.getElementById("sortableTable");
     let workbook = XLSX.utils.table_to_book(table_elt);
 
@@ -7,5 +7,5 @@ function extractXLSX(){
     XLSX.utils.sheet_add_aoa(ws, [["Created on "+new Date().toISOString()]], {origin:-1});
 
 // Package and Release Data (`writeFile` tries to write and save an XLSB file)
-    XLSX.writeFile(workbook, "Report.xlsb");
+    XLSX.writeFile(workbook, "Time_Sheet_" + name + "_" + month + "_" + year + ".xlsb");
 }
