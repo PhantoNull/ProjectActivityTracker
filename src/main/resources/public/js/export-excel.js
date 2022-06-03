707d183cdfbda3c688b32a35c997608d5bc5126e
PhantoNull <l.dipierro@campus.unimib.it>
2022-06-03 15:32:52 +0200

    Aggiunta funzione export - non completa

---
+++ src/main/resources/public/js/export-excel.js
@@ -0,0 +1,11 @@
+function extractXLSX(){
+    let table_elt = document.getElementById("sortableTable");
+    let workbook = XLSX.utils.table_to_book(table_elt);
+
+// Process Data (add a new row)
+    let ws = workbook.Sheets["Time Sheet"];
+    XLSX.utils.sheet_add_aoa(ws, [["Created on "+new Date().toISOString()]], {origin:-1});
+
+// Package and Release Data (`writeFile` tries to write and save an XLSB file)
+    XLSX.writeFile(workbook, "Report.xlsb");
+}
